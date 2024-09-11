e1d62413050f2c27443b5a0933b8df40907ef038
gmnzor <gomez017@gmail.com>
2024-09-10 19:09:46 -0700

    uploading collated query data with wage and CPI data

---
+++ Nestor/SQL/Schema v3.sql
@@ -0,0 +1,58 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "layoffs_data" (
+    "date" date   NOT NULL,
+    "laid_off_by_month" int   NOT NULL,
+    "funds_raised_by_month" float   NOT NULL,
+    CONSTRAINT "pk_layoffs_data" PRIMARY KEY (
+        "date"
+     )
+);
+
+CREATE TABLE "mortgage_rate_data" (
+    "date" date   NOT NULL,
+    "mortgage_rate" float   NOT NULL
+);
+
+CREATE TABLE "stock_data" (
+    "date" date   NOT NULL,
+    "stock_open" float   NOT NULL,
+    "stock_high" float   NOT NULL,
+    "stock_low" float   NOT NULL,
+    "stock_close" float   NOT NULL,
+    "stock_adj_close" float   NOT NULL,
+    "stock_volume" int   NOT NULL
+);
+
+CREATE TABLE "unemployment_rate_data" (
+    "date" date   NOT NULL,
+    "unemployment_rate" float   NOT NULL
+);
+
+CREATE TABLE "fed_interest_rate_data" (
+    "date" date   NOT NULL,
+    "fed_rate" float   NOT NULL
+);
+
+CREATE TABLE "consumer_price_index_data" (
+    "date" date   NOT NULL,
+    "education_and_communication_commodities_inflation" float   NOT NULL,
+    "education_and_communication_services_inflation" float   NOT NULL,
+    "information_technology_commodities_inflation" float   NOT NULL,
+    "education_and_communication_commodities_avg_value" float   NOT NULL,
+    "education_and_communication_services_avg_value" float   NOT NULL,
+    "information_technology_commodities_avg_value" float   NOT NULL
+);
+
+CREATE TABLE "wages_data" (
+    "date" date   NOT NULL,
+    "benefits_inflation" float   NOT NULL,
+    "total_compensation_inflation" float   NOT NULL,
+    "wages_and_salaries_inflation" float   NOT NULL,
+    "benefits_avg_value" float   NOT NULL,
+    "total_compensation_avg_value" float   NOT NULL,
+    "wages_and_salaries_avg_value" float   NOT NULL
+);
+
